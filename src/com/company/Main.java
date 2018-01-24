4edb451a21b017f85c967a1772dae2d66655f83f
Conner Caspar <conner.caspar@gmail.com>
2018-01-24 16:33:35 -0500

    Initial notes

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,29 @@ public class Main {
 
     public static void main(String[] args)
     {
-        
+        // > greater than
+        // < less than
+        // >= greater than or equal to
+        // <= less than or equal to
+        // != not equal to
+        // == is equal to
+        // ! not
+        // || or
+        // && and
+
+        int a = 2;
+        int b = 4;
+        int x = 6;
+        int y = 1;
+
+        System.out.println("a = 2");
+        System.out.println("b = 4");
+        System.out.println("x = 6");
+        System.out.println("y = 1");
+
+        if (a == x && y + y == a)
+        {
+            System.out.println("a == x && y + y == a");
+        }
     }
 }
